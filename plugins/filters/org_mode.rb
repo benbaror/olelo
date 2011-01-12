74c7c7d513e6fb472d69a3343f0fbdf1d8eff2ee
Ben <Ben@MacBook>
2011-01-12 16:21:25 +0200

    Workaround the // replacement of rubypants in the org-ruby to_html

--- plugins/filters/org_mode.rb
+++ plugins/filters/org_mode.rb
@@ -2,5 +2,5 @@ description 'Emacs org-mode filter'
 require 'org-ruby'
 
 Filter.create :orgmode do |context, content|
-  Orgmode::Parser.new(content).to_html
+  Orgmode::Parser.new(content.gsub("\\\\","\\\\\\\\\\\\\\\\")).to_html
 end
